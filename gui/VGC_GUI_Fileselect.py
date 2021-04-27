3f6a2b427882b4bd9a24eddec4cbe6b791dd00cd
x211321 <49248821+x211321@users.noreply.github.com>
2021-04-27 14:43:48 +0200

    Renames combine-checkbutton, updates TODO

--- gui/VGC_GUI_Fileselect.py
+++ gui/VGC_GUI_Fileselect.py
@@ -26,7 +26,7 @@ class GUI_File(Frame):
     def init(self):
         self.file_select_text  = Label_(self, anchor="e", padx=5, text="Active VGC file:")
         self.file_select       = Combobox_(self, width=40, values=[os.path.basename(x) for x in glob.glob(VAR.DATA_PATH+VAR.FILE_PREFIX+"*.csv")])
-        self.combine_platforms = Checkbutton_(self, padx=8, label="Combine similar platforms", default=True, command=self.setCurrentVGCFile)
+        self.combine_platforms = Checkbutton_(self, padx=8, label="Combine related platforms", default=True, command=self.setCurrentVGCFile)
 
         self.combine_platforms.pack(side=RIGHT)
         self.file_select.pack(side=RIGHT)
