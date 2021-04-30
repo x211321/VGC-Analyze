d68926f4bc0870b4844124cb8ed486b2bf6104cb
x211321 <49248821+x211321@users.noreply.github.com>
2021-04-30 13:38:00 +0200

    Changes json encoding to utf-8

--- VGC_Json.py
+++ VGC_Json.py
@@ -9,7 +9,7 @@ import json
 def readJson(fileName):
     result = {}
     if os.path.exists(fileName) and os.path.getsize(fileName) > 0:
-        file = open(fileName, "r")
+        file = open(fileName, "r", encoding="utf-8")
         result = json.load(file)
         file.close()
 
@@ -24,6 +24,6 @@ def writeJson(data, fileName):
     if not os.path.exists(os.path.dirname(fileName)):
         os.makedirs(os.path.dirname(fileName))
 
-    file = open(fileName, "w")
+    file = open(fileName, "w", encoding="utf-8")
     json.dump(data, file, sort_keys=True, indent="\t")
     file.close()
