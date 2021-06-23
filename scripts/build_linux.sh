8198d4b78488987b73845dcb65c37d23430a23db
x211321 <49248821+x211321@users.noreply.github.com>
2021-06-23 14:52:36 +0200

    Update build scrips

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -1,26 +1,31 @@
 
 #!/bin/bash
 
+# Create virual environment
 mkdir ./build
 cd ./build
 mkdir ./virtual_env
 
 python3 -m venv ./virtual_env
 
+# Activate virual environment
 export PATH=$PWD/virtual_env/bin:$PATH
 
+# Install dependencies
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade Pillow
 python3 -m pip install --upgrade matplotlib
 python3 -m pip install --upgrade pyinstaller
 python3 -m pip install --upgrade polib
 
+# Cleanup dependencies to reduce the file size of the executable
 rm -Rf ./virtual_env/lib/python*.*/site-packages/matplotlib/mpl-data/fonts
 rm -Rf ./virtual_env/lib/python*.*/site-packages/matplotlib/mpl-data/images
 rm -Rf ./virtual_env/lib/python*.*/site-packages/matplotlib/mpl-data/plot_directive
 rm -Rf ./virtual_env/lib/python*.*/site-packages/matplotlib/mpl-data/sample_data
 rm -Rf ./virtual_env/lib/python*.*/site-packages/matplotlib/mpl-data/stylelib
 
-#pyinstaller --noconfirm --windowed --add-data $PWD/../../assets:assets --icon=$PWD/../../assets/icons/icon.ico ../../VGC_Analyze.py
+# Run Pyinstaller
 pyinstaller --onefile --windowed --add-data $PWD/../../assets:assets --icon=$PWD/../../assets/icons/icon.ico --hidden-import='PIL._tkinter_finder' ../../VGC_Analyze.py
 
+#pyinstaller --noconfirm --windowed --add-data $PWD/../../assets:assets --icon=$PWD/../../assets/icons/icon.ico ../../VGC_Analyze.py
--- scripts/build_windows.bat
+++ scripts/build_windows.bat
@@ -7,7 +7,7 @@ mkdir .\virtual_env
 python -m venv .\virtual_env
 
 REM Activate virual environment
-SET PATH=%CD%\virtual_env\Scripts;%PATH%
+set PATH=%CD%\virtual_env\Scripts;%PATH%
 
 REM Install dependencies
 python -m pip install --upgrade pip
@@ -16,13 +16,14 @@ python -m pip install --upgrade matplotlib
 python -m pip install --upgrade pyinstaller
 python -m pip install --upgrade polib
 
-REM Cleanup dependencies
+REM Cleanup dependencies to reduce the file size of the executable
 rmdir /S /Q .\virtual_env\Lib\site-packages\matplotlib\mpl-data\fonts
 rmdir /S /Q .\virtual_env\Lib\site-packages\matplotlib\mpl-data\images
 rmdir /S /Q .\virtual_env\Lib\site-packages\matplotlib\mpl-data\plot_directive
 rmdir /S /Q .\virtual_env\Lib\site-packages\matplotlib\mpl-data\sample_data
 rmdir /S /Q .\virtual_env\Lib\site-packages\matplotlib\mpl-data\stylelib
 
-pyinstaller --noconfirm --windowed --add-data "%CD%\..\..\assets;assets" --icon="%CD%\..\..\assets\icons\icon.ico" ..\..\VGC_Analyze.py
+REM Run Pyinstaller
 pyinstaller --onefile --windowed --add-data "%CD%\..\..\assets;assets" --icon="%CD%\..\..\assets\icons\icon.ico" ..\..\VGC_Analyze.py
-@REM pyinstaller ..\..\VGC_Analyze.py
+
+REM pyinstaller --noconfirm --windowed --add-data "%CD%\..\..\assets;assets" --icon="%CD%\..\..\assets\icons\icon.ico" ..\..\VGC_Analyze.py
