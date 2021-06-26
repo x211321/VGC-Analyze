0a588e36ed611bef7f32b2fc1f505e8d6af40c24
x211321 <49248821+x211321@users.noreply.github.com>
2021-06-26 21:36:18 +0200

    Update GUI_Menu.py

--- gui/GUI_Menu.py
+++ gui/GUI_Menu.py
@@ -1,3 +1,4 @@
+import platform
 import lib.Settings as settings
 from lib.Locale import _
 from lib.Browser import openFileManager
@@ -10,53 +11,62 @@ from tkinter import *
 # initMainMenu
 # --------------------
 def initMainMenu(gui):
-    gui.main_menu = Menu(gui, bg=VAR.GUI_COLOR_SECONDARY)
+    if platform.system() == "Darwin":
+        bg        = None
+        bg_active = None
+        fg_active = None
+    else:
+        bg        = VAR.GUI_COLOR_SECONDARY
+        bg_active = VAR.MENU_BG_HOVER
+        fg_active = VAR.MENU_FG_HOVER
+
+    gui.main_menu = Menu(gui, bg=bg)
 
     # File menu
-    gui.file_menu = Menu(gui.main_menu, tearoff=0, bg=VAR.GUI_COLOR_SECONDARY,
-                                                   activebackground=VAR.MENU_BG_HOVER,
-                                                   activeforeground=VAR.MENU_FG_HOVER)
+    gui.file_menu = Menu(gui.main_menu, tearoff=0, bg=bg,
+                                                   activebackground=bg_active,
+                                                   activeforeground=fg_active)
     gui.file_menu.add_command(label=_("Settings"), command=gui.showSettings)
     gui.file_menu.add_command(label=_("Open data folder"), command=lambda:openFileManager(VAR.DATA_PATH))
     gui.file_menu.add_command(label=_("Quit"), command=gui.destroy, accelerator="Ctrl+Q")
 
     # Search menu
-    gui.search_menu = Menu(gui.main_menu, tearoff=0, bg=VAR.GUI_COLOR_SECONDARY,
-                                                     activebackground=VAR.MENU_BG_HOVER,
-                                                     activeforeground=VAR.MENU_FG_HOVER)
+    gui.search_menu = Menu(gui.main_menu, tearoff=0, bg=bg,
+                                                     activebackground=bg_active,
+                                                     activeforeground=fg_active)
     gui.search_menu.add_command(label=_("Search for item"), command=gui.pop_itemSearch.show, accelerator="Ctrl+F")
 
     # Download menu
-    gui.download_menu = Menu(gui.main_menu, tearoff=0, bg=VAR.GUI_COLOR_SECONDARY,
-                                                       activebackground=VAR.MENU_BG_HOVER,
-                                                       activeforeground=VAR.MENU_FG_HOVER)
+    gui.download_menu = Menu(gui.main_menu, tearoff=0, bg=bg,
+                                                       activebackground=bg_active,
+                                                       activeforeground=fg_active)
     gui.download_menu.add_command(label=_("Download collection"), command=gui.pop_collectionDownload.show, accelerator="Ctrl+D")
 
     # Templates menu
-    gui.templates_menu = Menu(gui.main_menu, tearoff=0, bg=VAR.GUI_COLOR_SECONDARY,
-                                                        activebackground=VAR.MENU_BG_HOVER,
-                                                        activeforeground=VAR.MENU_FG_HOVER)
+    gui.templates_menu = Menu(gui.main_menu, tearoff=0, bg=bg,
+                                                        activebackground=bg_active,
+                                                        activeforeground=fg_active)
     gui.templates_menu.add_command(label=_("Manage templates"), command=gui.pop_templateManager.show, accelerator="Ctrl+B")
     generateTemplateMenu(gui)
 
     # Export menu
-    gui.export_menu = Menu(gui.main_menu, tearoff=0, bg=VAR.GUI_COLOR_SECONDARY,
-                                                     activebackground=VAR.MENU_BG_HOVER,
-                                                     activeforeground=VAR.MENU_FG_HOVER)
+    gui.export_menu = Menu(gui.main_menu, tearoff=0, bg=bg,
+                                                     activebackground=bg_active,
+                                                     activeforeground=fg_active)
     gui.export_menu.add_command(label=_("Export HTML"), command=gui.export, accelerator="Ctrl+E")
 
     # Window menu
-    gui.window_menu = Menu(gui.main_menu, tearoff=0, bg=VAR.GUI_COLOR_SECONDARY,
-                                                     activebackground=VAR.MENU_BG_HOVER,
-                                                     activeforeground=VAR.MENU_FG_HOVER)
+    gui.window_menu = Menu(gui.main_menu, tearoff=0, bg=bg,
+                                                     activebackground=bg_active,
+                                                     activeforeground=fg_active)
     gui.window_menu.add_command(label=_("Filters"), command=gui.toggleFilterFrame, accelerator="Alt+F")
     gui.window_menu.add_command(label=_("Item info"), command=gui.toggleItemInfoFrame, accelerator="Alt+I")
     gui.window_menu.add_command(label=_("Graphs"), command=gui.toggleGraphFrame, accelerator="Alt+G")
 
     # About menu
-    gui.about_menu = Menu(gui.main_menu, tearoff=0, bg=VAR.GUI_COLOR_SECONDARY,
-                                                    activebackground=VAR.MENU_BG_HOVER,
-                                                    activeforeground=VAR.MENU_FG_HOVER)
+    gui.about_menu = Menu(gui.main_menu, tearoff=0, bg=bg,
+                                                    activebackground=bg_active,
+                                                    activeforeground=fg_active)
     gui.about_menu.add_command(label=_("About VGC Analyzer"), command=gui.showAbout)
 
     gui.main_menu.add_cascade(label=_("File")     , menu=gui.file_menu)
