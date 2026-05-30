6657482109e7a61865720cb8cf6970a09ecce839
x211321 <49248821+x211321@users.noreply.github.com>
2026-05-30 18:56:59 +0200

    Fixes compatibility with older pillow versions

--- lib/Img.py
+++ lib/Img.py
@@ -22,12 +22,16 @@ def loadImage(path, width = 0, padx = 0):
 
     if pillow_available:
         if os.path.exists(path):
+
             img = Image.open(path)
 
             percent = width/float(img.size[0])
             height  = int(float(img.size[1]) * float(percent))
 
-            img = img.resize((width, height), Image.Resampling.LANCZOS)
+            if (int(Image.__version__.split(".")[0]) > 10):
+                img = img.resize((width, height), Image.Resampling.LANCZOS)
+            else:
+                img = img.resize((width, height), Image.ANTIALIAS)
 
             if padx:
                 img = padImage(img, width, height, padx)
@@ -64,7 +68,11 @@ def loadIcon(name, width, height, padx = 0):
     if pillow_available:
         if os.path.exists(iconPath):
             icon = Image.open(iconPath)
-            icon = icon.resize((width, height), Image.Resampling.LANCZOS)
+
+            if (int(Image.__version__.split(".")[0]) > 10):
+                icon = icon.resize((width, height), Image.Resampling.LANCZOS)
+            else:
+                icon = icon.resize((width, height), Image.ANTIALIAS)
 
             if padx:
                 icon = padImage(icon, width, height, padx)
